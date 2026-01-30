6e2c1d2835ff5a8b1f5dd7257541a67674b69208
Harish Rangadhama <harish.rangadhama@futureos.ae>
2026-01-30 13:11:15 +0400

    Deployment fix

--- build_images.sh
+++ build_images.sh
@@ -8,11 +8,11 @@ ARCH=arm64
 SAVE_IMAGE=true
 
 echo -e "\033[34mBuilding Docker image...\033[0m"
-docker build -f deployment/build/Dockerfile --tag trvision/ui:$ARCH . --no-cache
+docker build -f deployment/build/Dockerfile --tag assetai/ui:$ARCH . --no-cache
 
 if [ "$SAVE_IMAGE" = true ]; then
     echo -e "\033[33mSaving Docker image to tar file...\033[0m"
-    docker save -o deployment/docker/ui_$ARCH.tar trvision/ui:$ARCH
+    docker save -o deployment/docker/ui_$ARCH.tar assetai/ui:$ARCH
     echo -e "\033[32mDocker image saved successfully!\033[0m"
 fi
 
